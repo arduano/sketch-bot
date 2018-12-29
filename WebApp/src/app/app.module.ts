0423d339d09a8cb0efbe90863b707c4d4350f5fc
arduano <leonid.shchurov@gmail.com>
2018-12-30 03:06:51 +1100

    removed useless module

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SketchPageComponent } from './pages/sketch-page/sketch-page.component';
-import { StorageServiceModule } from 'angular-webstorage-service';
 
 @NgModule({
   declarations: [
@@ -14,8 +13,7 @@ import { StorageServiceModule } from 'angular-webstorage-service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    StorageServiceModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
