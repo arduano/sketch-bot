d5caf6f35b317e23b5a42ca445fdb4b0c1d52917
arduano <leonid.shchurov@gmail.com>
2019-04-27 16:48:14 +1000

    removed async child

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
 const gulp = require('gulp');
-const join = require('async-child-process').join;
 const fs = require('fs');
 const exec = require('child_process');
+
+function join(child) {
+  return new Promise((_resolve, _reject) => {
+    function resolve(result) {
+      child.removeAllListeners()
+      _resolve(result)
+    }
+    function reject(error) {
+      child.removeAllListeners()
+      _reject(error)
+    }
+    child.on('exit', (code, signal) => {
+      if (code != null && code !== 0) {
+        const error = new Error(`process exited with code ${code}`)
+        const flowWorkaround = error
+        flowWorkaround.code = code
+        flowWorkaround.signal = null
+        reject(error)
+      }
+      else if (signal) {
+        const error = new Error(`process exited with signal ${signal}`)
+        const flowWorkaround = error
+        flowWorkaround.code = null
+        flowWorkaround.signal = signal
+        reject(error)
+      }
+      else resolve({code, signal})
+    })
+    child.on('error', reject)
+  })
+}
 
 async function pRun(command) {
   let run = exec.exec(command);
@@ -17,7 +47,6 @@ async function pRun(command) {
   run.on('exit', function (code) {
     //console.log('child process exited with code ' + code.toString());
   });
-
   await join(run)
 }
 
