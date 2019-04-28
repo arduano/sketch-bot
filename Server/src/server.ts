9e396252356dcc3e7a8c705d2078997f8ec0b8de
arduano <leonid.shchurov@gmail.com>
2019-04-29 01:00:15 +1000

    settings file again

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -2,13 +2,13 @@ import { WebApi } from './webapi';
 import { DiscordBot } from './bot';
 import * as fs from 'fs';
 
-//const settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+const settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
 
-const settings: any = {
-    "port": process.env.PORT,
-    "token": process.env.TOKEN,
-    "baseUrl": process.env.URL
-}
+// const settings: any = {
+//     "port": process.env.PORT,
+//     "token": process.env.TOKEN,
+//     "baseUrl": process.env.URL
+// }
 
 const bot = new DiscordBot();
 bot.start(settings.token, settings.baseUrl, settings.port);
